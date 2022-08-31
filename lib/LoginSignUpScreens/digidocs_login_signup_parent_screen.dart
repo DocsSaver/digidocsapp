01182aa1c3b32320001ef56e76fd8ec4e69d77a5
DigiDocs <digidocs2022@gmail.com>
2022-08-31 23:19:29 +0530

    add files

---
+++ lib/LoginSignUpScreens/digidocs_login_signup_parent_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LoginSignUpParentScreen extends StatefulWidget {
+  const LoginSignUpParentScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginSignUpParentScreen> createState() => _LoginSignUpParentScreenState();
+}
+
+class _LoginSignUpParentScreenState extends State<LoginSignUpParentScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
